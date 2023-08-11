41b9ed76d0a7182b2673d1a0246663b32ea17027
DESKTOP-L9KFTU4\sosik <dima.abramiuk@gmail.com>
2023-08-11 13:58:59 +0300

    1-3 Task completed

--- src/main/java/org/example/Task_1/HttpTask.java
+++ src/main/java/org/example/Task_1/HttpTask.java
@@ -1,8 +1,6 @@
 package org.example.Task_1;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
@@ -12,10 +10,15 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.example.human.Human;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class HttpTask {
     private static final String API_URL = "https://jsonplaceholder.typicode.com/users";
@@ -30,6 +33,8 @@ public class HttpTask {
                 + "\nResponce body: "
                 + EntityUtils.toString(httpResponse.getEntity());
     }
+
+    //=======================================TASK_1================================================================
 
     public static String sendGet() throws IOException {
         HttpClient httpClient = HttpClients.createDefault();
@@ -63,9 +68,7 @@ public class HttpTask {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
 
         HttpPut httpPut = new HttpPut(API_URL + "/" + jsonObject.get("id").getAsString());
-
         httpPut.setEntity(new StringEntity(jsonHuman));
-
         httpPut.setHeader("Content-Type", "application/json");
 
         return getResponceBody(httpClient.execute(httpPut));
@@ -86,7 +89,7 @@ public class HttpTask {
         return getResponceBody(httpClient.execute(httpDelete));
     }
 
-    public static String sendGetByIndex(String id) throws IOException {
+    public static String sendGetByIndex(int id) throws IOException {
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(API_URL + "/" + id);
 
@@ -98,5 +101,74 @@ public class HttpTask {
         HttpGet httpGet = new HttpGet(API_URL + "?username=" + username);
 
         return getResponceBody(httpClient.execute(httpGet));
+    }
+
+    //=======================================TASK_2================================================================
+    private static void toJson(int id, int postId, HttpResponse comments) throws IOException {
+        String pathOfNewFile = "src/user-" + id + "-post-" + postId + "-comments.json";
+
+        new File(pathOfNewFile).createNewFile();
+        File toFile = new File(pathOfNewFile);
+
+        try (FileWriter fw = new FileWriter(toFile)) {
+            fw.write(EntityUtils.toString(comments.getEntity()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getLastPostIndex(HttpResponse response) throws IOException {
+        String json = EntityUtils.toString(response.getEntity());
+        Gson gson = new Gson();
+
+        JsonArray postsArray = gson.fromJson(json, JsonArray.class);
+
+        try {
+            JsonObject lastPost = postsArray.get(postsArray.size() - 1).getAsJsonObject();
+            return lastPost.get("id").getAsInt();
+        } catch (RuntimeException e){
+            return -1;
+        }
+    }
+
+    public static void sendGetLastPostComments(int id) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGetPosts = new HttpGet(API_URL + "/" + id + "/posts");
+
+        HttpResponse postsOfUserResponse = httpClient.execute(httpGetPosts);
+        int lastPostId = getLastPostIndex(postsOfUserResponse);
+
+        if(lastPostId==-1)
+            throw new RuntimeException("There isn't user with current id");
+
+        HttpGet httpGetComments = new HttpGet(API_URL.replace("users", "posts/" + lastPostId + "/comments"));
+
+        HttpResponse commentsResponse = httpClient.execute(httpGetComments);
+        toJson(id, lastPostId, commentsResponse);
+    }
+
+    //=======================================TASK_3================================================================
+
+    private static String parseTodos(HttpResponse todosOfUserResponse) throws IOException {
+        String json = EntityUtils.toString(todosOfUserResponse.getEntity());
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonArray todosArray = gson.fromJson(json, JsonArray.class);
+        JsonArray openedTodosArray = new JsonArray();
+
+        for(JsonElement obj : todosArray){
+            if(!obj.getAsJsonObject().get("completed").getAsBoolean())
+                openedTodosArray.add(obj);
+        }
+
+        return gson.toJson(openedTodosArray);
+    }
+
+    public static String sendGetAllOpenedTodos(int id) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGetAllTodos = new HttpGet(API_URL + "/" + id + "/todos");
+
+        HttpResponse todosOfUserResponse = httpClient.execute(httpGetAllTodos);
+
+        return parseTodos(todosOfUserResponse);
     }
 }
--- src/main/java/org/example/Task_1/HttpTaskTest.java
+++ src/main/java/org/example/Task_1/HttpTaskTest.java
@@ -34,12 +34,35 @@ public class HttpTaskTest {
         System.out.println("=============================GET BY INDEX============================");
         System.out.println("=====================================================================");
 
-        System.out.println("send GET (By Index) = " + HttpTask.sendGetByIndex("9"));
+        System.out.println("send GET (By Index) = " + HttpTask.sendGetByIndex(9));
 
         System.out.println("=====================================================================");
         System.out.println("==========================GET BY USERNAME============================");
         System.out.println("=====================================================================");
 
         System.out.println("send GET (By Username) = " + HttpTask.sendGetByUsername("Delphine"));
+
+        System.out.println("=====================================================================");
+        System.out.println("===========WRITE ALL COMMENTS FROM LAST POST TO A FILE===============");
+        System.out.println("=====================================================================");
+
+        HttpTask.sendGetLastPostComments(1);
+        HttpTask.sendGetLastPostComments(2);
+        HttpTask.sendGetLastPostComments(3);
+        HttpTask.sendGetLastPostComments(4);
+        HttpTask.sendGetLastPostComments(5);
+        HttpTask.sendGetLastPostComments(6);
+        HttpTask.sendGetLastPostComments(7);
+        HttpTask.sendGetLastPostComments(8);
+        HttpTask.sendGetLastPostComments(9);
+        HttpTask.sendGetLastPostComments(10);
+        //Exception (there isn't user with current id)
+        //HttpTask.sendGetLastPostComments(11);
+
+        System.out.println("=====================================================================");
+        System.out.println("===================PRINT ALL OPENED TODOS OF USER====================");
+        System.out.println("=====================================================================");
+
+        System.out.println("HttpTask.sendGetAllOpenedTodos(9) = " + HttpTask.sendGetAllOpenedTodos(9));
     }
 }
